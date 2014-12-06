5c62a7198255ddff11082bc6fbacbaab0a31116c
ataro24 <touch.taro@gmail.com>
2014-12-06 15:39:29 +0900

    add story info and data transfer

---
+++ tool/data_transfer.rb
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+require 'json'
+ROOT     = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+src_dir  = "#{ROOT}/tool/resource"
+dist_dir = "#{ROOT}/data"
+
+SCENE    = "---scene-"
+QUESTION = "----question"
+ANSWER   = "----answer"
+COMMENT  = "----comment"
+
+# return chunk = {scene_id => [line,....]}
+def split_scene(str)
+  line_list = str.split("\n").map{|s| s.strip}
+  chunk = {}
+  before_key = ""
+  line_list.each do |line|
+    if line =~ /^#{SCENE}.*/
+      before_key = line.gsub(/#{SCENE}/, '')
+      chunk[before_key] = []
+    else
+      chunk[before_key] << line unless line == ""
+    end
+  end
+  chunk
+end
+
+def separate_scene_info(scene_info)
+  ret = {
+    "serif"    => [],
+    "question" => [],
+    "select"   => {},
+    "answer"   => "",
+    "comment"  => []
+  }
+  before_key = "serif"
+  scene_info.each_with_index do |line, i|
+    if line =~ /^#{QUESTION}/
+      before_key = "question"
+    elsif line =~ /^#{ANSWER}/
+      before_key = "answer"
+    elsif line =~ /^#{COMMENT}/
+      before_key = "comment"
+    else
+      case before_key
+      when "serif", "question", "comment"
+        ret[before_key] << line
+      when "select"
+        key, value = line.split(" ")
+        ret[before_key][key] = value
+      when "answer"
+        ret[before_key] = line
+      else
+        puts "error"
+      end
+    end
+  end
+  ret
+end
+
+
+
+# src_dir以下のファイル一覧取得
+file_list = Dir.glob("#{src_dir}/*")
+
+# データファイルを読み込み、parseする
+file_list.each do |file|
+  next unless File.file?(file)
+  src = File.open(file).read
+
+  ret = {}
+  scene_list = split_scene(src)
+  scene_list.each do |id, info|
+    ret[id] = separate_scene_info(info)
+  end
+
+  output_file = dist_dir + "/" + file.split("/").last.gsub(/\.txt/, '') + ".json"
+  File.write(output_file, JSON.generate(ret))
+end
+
+
+
+
+
