4650b1a4180ffe4ae780b2f9c1063f098f3d441c
itomo <lalburiki@gmail.com>
2014-12-07 10:48:37 +0900

    cut in

--- js/define.js
+++ js/define.js
@@ -22,6 +22,7 @@ var TITLE_IMG_DIR  = IMG_DIR + "/title";
 var SELECT_IMG_DIR = IMG_DIR + "/select";
 var STAGE_IMG_DIR  = IMG_DIR + "/stage";
 
+var TITLE_IMG_TEXT = TITLE_IMG_DIR + "/font_01.png";
 var TITLE_IMG_BACKGROUND = TITLE_IMG_DIR + "/bg_01.jpg";
 var TITLE_IMG_CHARACTER  = TITLE_IMG_DIR + "/chara_01.png";
 var TITLE_IMG_BUTTON     = TITLE_IMG_DIR + "/button_01.png";
@@ -35,5 +36,7 @@ var SELECT_IMG_TOUCH_1 = SELECT_IMG_DIR + "/SelectScene_06.png";
 var SELECT_IMG_TOUCH_2 = SELECT_IMG_DIR + "/SelectScene_07.png";
 var SELECT_IMG_TOUCH_3 = SELECT_IMG_DIR + "/SelectScene_08.png";
 var SELECT_IMG_TOUCH_4 = SELECT_IMG_DIR + "/SelectScene_09.png";
+
+var STAGE_IMG_CUTIN = STAGE_IMG_DIR + "/cutin.png";
 
 var KOBI_SOUND = SOUND_DIR + "/cobi.mp3";
